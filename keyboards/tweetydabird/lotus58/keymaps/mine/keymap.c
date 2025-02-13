5add13e40d053ef429bed01d0f020d701e6bc465
xiaodong.jia <xiaodong.jia@bytedance.com>
2025-02-13 17:16:01 +0800

    fix sleep flickering

--- keyboards/tweetydabird/lotus58/keymaps/mine/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/mine/keymap.c
@@ -1,7 +1,7 @@
 // Copyright 2022 Markus Knutsson (@TweetyDaBird)
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
-#include "ocean_dream.h"
+#include "../../ocean_dream.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MPLY, KC_MPLY, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, MO(3), MO(3), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_EQL, KC_LGUI, MO(1), KC_LALT, KC_SPC, SC_SENT, KC_BSPC, MO(1), MO(2)),
                                                               [1] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS, KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F12, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PEQL, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_LT, KC_GT, KC_PPLS, KC_PMNS, KC_PAST, KC_PSLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS),
@@ -13,21 +13,8 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {[0] = {ENC
 #endif
 
 #ifdef OLED_ENABLE
-/*
-static void render_anim(void) {
-    if (first_render) {
-        oled_write_raw_P(frame, ANIM_SIZE);
-        first_render = 0;
-    } else {
-        change_frame_bytewise(c_frame);
-    }
-    c_frame = c_frame + 1 > IDLE_FRAMES ? 0 : c_frame + 1;
-}
-*/
-
 bool oled_task_user(void) {
     // Render the OLED
-    // render_anim();
     render_stars();
     return false;
 }
