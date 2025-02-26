417badcf85daf3eb0db3fce7ca1b1e2bef51a349
xiaodong.jia <xiaodong.jia@bytedance.com>
2025-02-26 14:16:13 +0800

    add macros and combos for pairs

--- keyboards/tweetydabird/lotus58/keymaps/mine/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/mine/keymap.c
@@ -1,8 +1,72 @@
 // Copyright 2022 Markus Knutsson (@TweetyDaBird)
 // SPDX-License-Identifier: GPL-2.0-or-later
-#include QMK_KEYBOARD_H
 #include "../../ocean_dream.h"
 #include "out.h"
+#include "keymap.h"
+
+enum combo_events {
+    COMBO_EVENT_BRACES,
+    COMBO_EVENT_BRACKETS,
+    COMBO_EVENT_SQ_BRACKETS,
+    COMBO_EVENT_ANG_BRACKETS,
+};
+
+const uint16_t PROGMEM braces_combo[]      = {KC_LCBR, KC_RCBR, COMBO_END};
+const uint16_t PROGMEM brackets_combo[]    = {KC_LPRN, KC_RPRN, COMBO_END};
+const uint16_t PROGMEM sqbrackets_combo[]  = {KC_LBRC, KC_RBRC, COMBO_END};
+const uint16_t PROGMEM angbrackets_combo[] = {KC_LABK, KC_RABK, COMBO_END};
+
+combo_t key_combos[] = {
+    [COMBO_EVENT_BRACES]       = COMBO_ACTION(braces_combo),      //
+    [COMBO_EVENT_BRACKETS]     = COMBO_ACTION(brackets_combo),    //
+    [COMBO_EVENT_SQ_BRACKETS]  = COMBO_ACTION(sqbrackets_combo),  //
+    [COMBO_EVENT_ANG_BRACKETS] = COMBO_ACTION(angbrackets_combo), //
+};
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+    switch (combo_index) {
+        case COMBO_EVENT_BRACES:
+            if (pressed) {
+                SEND_STRING("{}" SS_TAP(X_LEFT));
+            }
+            break;
+        case COMBO_EVENT_BRACKETS:
+            if (pressed) {
+                SEND_STRING("()" SS_TAP(X_LEFT));
+            }
+            break;
+        case COMBO_EVENT_SQ_BRACKETS:
+            if (pressed) {
+                SEND_STRING("[]" SS_TAP(X_LEFT));
+            }
+            break;
+        case COMBO_EVENT_ANG_BRACKETS:
+            if (pressed) {
+                SEND_STRING("<>" SS_TAP(X_LEFT));
+            }
+            break;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MACRO_GRAVE:
+            if (record->event.pressed) {
+                SEND_STRING("``" SS_TAP(X_LEFT));
+                break;
+            }
+        case MACRO_QUOTES:
+            if (record->event.pressed) {
+                if (get_mods() & MOD_MASK_SHIFT) {
+                    SEND_STRING("\"\"" SS_TAP(X_LEFT));
+                } else {
+                    SEND_STRING("''" SS_TAP(X_LEFT));
+                }
+                break;
+            }
+    }
+    return true;
+}
 
 const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
     {{0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5}, {5, 5}}, // Left hand
@@ -35,15 +99,3 @@ bool oled_task_user(void) {
 }
 
 #endif
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-#ifdef OCEAN_DREAM_ENABLE
-        case KC_LCTL:
-        case KC_RCTL:
-            is_calm = (record->event.pressed) ? true : false;
-            break;
-#endif
-    }
-    return true;
-}
