9c409b01e724fca9a3587474a40088153ac410e5
xiaodong.jia <xiaodong.jia@bytedance.com>
2025-03-09 20:12:32 +0800

    some organization

---
+++ keyboards/tweetydabird/lotus58/keymaps/mine/keymap.h
@@ -0,0 +1,7 @@
+#include QMK_KEYBOARD_H
+#pragma once
+
+enum custom_keycodes {
+    MACRO_GRAVE = SAFE_RANGE,
+    MACRO_QUOTES,
+};
--- keyboards/tweetydabird/lotus58/keymaps/mine/out.h
+++ keyboards/tweetydabird/lotus58/keymaps/mine/out.h
@@ -1,3 +1,4 @@
+#include QMK_KEYBOARD_H
 #if __has_include("keymap.h")
 #    include "keymap.h"
 #endif
